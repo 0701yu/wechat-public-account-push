fdc7cc076340a13fd257335cf16e983c6711679c
0701yu <125334416+0701yu@users.noreply.github.com>
2023-02-14 20:12:23 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -50,8 +50,6 @@ const USER_CONFIG = {
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
   CALLBACK_TEMPLATE_ID: 'cjThxfcmEIEQRWYHts0eip5dyafK5K93RxtXlWZ7NqY',
-  //节假日
-  holidaytts:'bala',
   CALLBACK_USERS: [
     {
       name: '自己',
