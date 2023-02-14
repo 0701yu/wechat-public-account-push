b24f5d46ce0e8b7cfafb3a934d2283e59956e346
0701yu <125334416+0701yu@users.noreply.github.com>
2023-02-14 13:58:39 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -51,7 +51,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '4HV2L72b6qcWiqSuJuVAlh6DmXhOOEjVB4tySg_wMYE',
+  CALLBACK_TEMPLATE_ID: 'ocGyt543CgeeQUMoHFSPVSkPC83o',
 
   CALLBACK_USERS: [
     {
